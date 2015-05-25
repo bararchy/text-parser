b0915b8c6162bc4e0ad29b0b4b6e1991e1217669
Bar Hofesh <bar.hofesh@safe-t.com>
2015-05-25 15:02:27 +0300

    Update text-parser_spec.cr

--- spec/text-parser_spec.cr
+++ spec/text-parser_spec.cr
@@ -1,8 +1,24 @@
 require "./spec_helper"
 
+text_test_colorless = ["Lorem ipsum dolor sit amet, liber eloquentiam quo no,",
+                      "usu at placerat hendrerit neglegentur.",
+                      "Et unum detracto quo, pri minimum constituam ne.",
+                      "Mei prima mnesarchum ne. Eum facilis expetenda ne."].join("\r\n")
+text_test_colored = ["Lorem ipsum dolor sit amet, liber eloquentiam quo no,##red",
+                     "usu at placerat hendrerit neglegentur.##green",
+                     "Et unum detracto quo, pri minimum constituam ne.##blue",
+                     "Mei prima mnesarchum ne. Eum facilis expetenda ne.##yellow"].join("\r\n")
+text_test_bold = ["Lorem ipsum dolor sit amet, liber eloquentiam quo no,##bold",
+                  "usu at placerat hendrerit neglegentur.##bold",
+                  "Et unum detracto quo, pri minimum constituam ne.##bold",
+                  "Mei prima mnesarchum ne. Eum facilis expetenda ne.##bold"].join("\r\n")
+text_test_options_enter = ["(enter)",
+                           "options(b,a)"].join("\r\n")
 describe Text::Parser do
-  # TODO: Write tests
-
+  Text::Parser::Box.parse(text_test_colorless)
+  Text::Parser::Box.parse(text_test_colored)
+  Text::Parser::Box.parse(text_test_bold)
+  Text::Parser::Box.parse(text_test_options_enter)
   it "works" do
     false.should eq(true)
   end
